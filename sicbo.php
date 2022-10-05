6490d0542ddd6ea37adbc20104e7350b2d54c77a
Dawson Helms <dhelm30700@gmail.com>
2022-10-04 20:02:11 -0400

    added

---
+++ sicbo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<head>
+    <title>Sicbo</title>
+</head>
+<body>
+    <form action="http://deltona.birdnest.org/~acc.helmsd7/sicbo.php" method="GET">
+        <label>Big Bet Amount: <input type="text" name="big"></label><br>
+        <label>Small Bet Amount: <input type="text" name="small"></label><br>
+        <label>Even Bet Amount: <input type="text" name="even"></label><br>
+        <label>Odd Bet Amount: <input type="text" name="odd"></label><br>
+
+        <button type="Submit">Submit</button>
+    </form>
